8f1191eb7568265973d52ed8b3d0e51e67cb55f4
Blythe Pollard <blytheepollard@gmail.com>
2019-11-12 22:53:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,8 @@ class Pokemon
   end
   
   def self.find(id)
-    
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Pokemon.new(result[0], result[1], result[2])
   end
 end

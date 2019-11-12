6a691e777edc36094e40eb52600913c8129e64d4
Blythe Pollard <blytheepollard@gmail.com>
2019-11-12 22:42:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
       INSERT INTO pokemon (name, type)
       VALUES (?, ?)
     SQL
-    db.execute(sql, self.name, self.grade)
+    db.execute(sql, self.name, self.type)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")
   end
 end
